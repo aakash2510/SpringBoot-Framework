39e2cee099517c01def7d4161f17d56a6928ff03
aakash2510 <aakashkumar2127@gmail.com>
2022-02-14 19:57:02 +0530

    first commit

---
+++ SpringFramework/src/com/spring/lecture1/Lecture1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.spring.lecture1;
+
+/**
+ * @author HP
+ *
+ */
+public class Lecture1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
